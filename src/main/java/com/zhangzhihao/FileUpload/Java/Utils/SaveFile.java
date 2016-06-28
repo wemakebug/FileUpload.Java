a1a985551b0b05fcf14a7375d7b6182c95689b65
Lemon2016 <18331503830@163.com>
2016-06-28 16:17:24 +0800

    修复了文件保存失败的问题

--- src/main/java/com/zhangzhihao/FileUpload/Java/Utils/SaveFile.java
+++ src/main/java/com/zhangzhihao/FileUpload/Java/Utils/SaveFile.java
@@ -2,27 +2,45 @@ package com.zhangzhihao.FileUpload.Java.Utils;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 
 public class SaveFile {
-    public static boolean saveFile(String localPath, String fileFullName, MultipartFile file) throws IOException {
+    public static boolean saveFile(String localPath, String fileFullName, MultipartFile file) throws Exception {
+        FileOutputStream outStream = null;
         try {
-            InputStream inputStream = file.getInputStream();
-            int available = inputStream.available();
-            byte[] inOutb = new byte[available];
-            int read = inputStream.read(inOutb);
-            File outFile = new File(localPath + "/src/main/webapp/upload/" + fileFullName);
-            FileOutputStream outStream = new FileOutputStream(outFile);
-            outStream.write(read);
-            inputStream.close();
-            outStream.close();
+            byte[] data = readInputStream(file.getInputStream());
+            //new一个文件对象用来保存图片，默认保存当前工程根目录
+            File imageFile = new File(localPath + "/src/main/webapp/upload/" + fileFullName);
+            //创建输出流
+            outStream = new FileOutputStream(imageFile);
+            //写入数据
+            outStream.write(data);
+            outStream.flush();
+
         } catch (Exception e) {
+            e.printStackTrace();
             throw e;
+        } finally {
+            outStream.close();
         }
         return true;
+    }
+
+    public static byte[] readInputStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        //创建一个Buffer字符串
+        byte[] buffer = new byte[1024];
+        //每次读取的字符串长度，如果为-1，代表全部读取完毕
+        int len = 0;
+        //使用一个输入流从buffer里把数据读取出来
+        while ((len = inStream.read(buffer)) != -1) {
+            //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
+            outStream.write(buffer, 0, len);
+        }
+        //关闭输入流
+        inStream.close();
+        //把outStream里的数据写入内存
+        return outStream.toByteArray();
     }
 }
