7ecd759f8e5a2e11b3064dce371bb6b443eaa77c
张志豪 <izhangzhihao@hotmail.com>
2016-07-01 20:33:53 +0800

    路径不存在会自动创建文件夹，使用try自动管理FileOutputStream

--- src/main/java/com/zhangzhihao/FileUpload/Java/Utils/SaveFile.java
+++ src/main/java/com/zhangzhihao/FileUpload/Java/Utils/SaveFile.java
@@ -2,45 +2,51 @@ package com.zhangzhihao.FileUpload.Java.Utils;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 
 public class SaveFile {
-    public static boolean saveFile(String localPath, String fileFullName, MultipartFile file) throws Exception {
-        FileOutputStream outStream = null;
-        try {
-            byte[] data = readInputStream(file.getInputStream());
-            //new一个文件对象用来保存图片，默认保存当前工程根目录
-            File imageFile = new File(localPath + "/src/main/webapp/upload/" + fileFullName);
-            //创建输出流
-            outStream = new FileOutputStream(imageFile);
-            //写入数据
-            outStream.write(data);
-            outStream.flush();
+	public static boolean saveFile(String localPath, String fileFullName, MultipartFile file) throws Exception {
+		byte[] data = readInputStream(file.getInputStream());
+		//new一个文件对象用来保存图片，默认保存当前工程根目录
+		File uploadFile = new File(localPath + "/src/main/webapp/upload/" + fileFullName);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        } finally {
-            outStream.close();
-        }
-        return true;
-    }
+		//判断文件夹是否存在，不存在就差创建一个
+		File fileDirectory = new File(localPath + "/src/main/webapp/upload/");
+		if (!fileDirectory.exists()) {
+			//noinspection ResultOfMethodCallIgnored
+			fileDirectory.mkdir();
+		}
 
-    public static byte[] readInputStream(InputStream inStream) throws Exception {
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        //创建一个Buffer字符串
-        byte[] buffer = new byte[1024];
-        //每次读取的字符串长度，如果为-1，代表全部读取完毕
-        int len = 0;
-        //使用一个输入流从buffer里把数据读取出来
-        while ((len = inStream.read(buffer)) != -1) {
-            //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
-            outStream.write(buffer, 0, len);
-        }
-        //关闭输入流
-        inStream.close();
-        //把outStream里的数据写入内存
-        return outStream.toByteArray();
-    }
+		//创建输出流
+		try (FileOutputStream outStream = new FileOutputStream(uploadFile)) {//写入数据
+			outStream.write(data);
+			outStream.flush();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return uploadFile.exists();
+	}
+
+	public static byte[] readInputStream(InputStream inStream) throws Exception {
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		//创建一个Buffer字符串
+		byte[] buffer = new byte[1024];
+		//每次读取的字符串长度，如果为-1，代表全部读取完毕
+		int len = 0;
+		//使用一个输入流从buffer里把数据读取出来
+		while ((len = inStream.read(buffer)) != -1) {
+			//用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
+			outStream.write(buffer, 0, len);
+		}
+		//关闭输入流
+		inStream.close();
+		//把outStream里的数据写入内存
+		return outStream.toByteArray();
+	}
 }
