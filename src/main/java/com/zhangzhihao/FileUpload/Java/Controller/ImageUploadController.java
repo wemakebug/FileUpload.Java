bd2983328d3cdb8662cdc58037ff66f96bc9b343
张志豪 <izhangzhihao@hotmail.com>
2016-07-15 17:27:06 +0800

    错误修复，冗余代码精简&&清理

--- src/main/java/com/zhangzhihao/FileUpload/Java/Controller/ImageUploadController.java
+++ src/main/java/com/zhangzhihao/FileUpload/Java/Controller/ImageUploadController.java
@@ -1,9 +1,7 @@
 package com.zhangzhihao.FileUpload.Java.Controller;
 
-import com.sun.javafx.css.CssError;
 import com.zhangzhihao.FileUpload.Java.Model.File;
 import com.zhangzhihao.FileUpload.Java.Service.FileService;
-import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,27 +9,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Path;
 import java.util.Date;
 import java.util.UUID;
 
-//import static com.sun.rowset.JdbcRowSetResourceBundle.fileName;
 import static com.zhangzhihao.FileUpload.Java.Utils.CreateMd5.createMd5;
 import static com.zhangzhihao.FileUpload.Java.Utils.DeepCopy.deepClone;
 import static com.zhangzhihao.FileUpload.Java.Utils.IsImag.isImage;
 import static com.zhangzhihao.FileUpload.Java.Utils.SaveFile.saveFile;
 
 
-/**
- * Created by LY on 2016/6/26.
- */
 @Controller
 @RequestMapping("/ImageUpload")
 public class ImageUploadController {
@@ -53,45 +40,29 @@ public class ImageUploadController {
                              @RequestParam("file") MultipartFile file) {
         String fileName = "";
 
-        MultipartFile saveFile=null;
+        MultipartFile saveFile = null;
 
         try {
-            saveFile= (MultipartFile) deepClone(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        java.io.File tempFile=new java.io.File(UUID.randomUUID().toString());
-        try {
+            saveFile = (MultipartFile) deepClone(file);
+            java.io.File tempFile = new java.io.File(UUID.randomUUID().toString());
             file.transferTo(tempFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            if (!isImage(tempFile))
+                return "{\"error\":true}";
 
-
-
-        if(!isImage(tempFile))
-            return "{\"error\":true}";
-
-
-
-        try {
             String path = ImageUploadController.class.getResource("/").getFile();
             int build = path.indexOf("build");
-            String realpath = path.substring(0, build)+"src/main/webapp/upload/";
+            String realpath = path.substring(0, build) + "src/main/webapp/upload/";
             String ext = name.substring(name.lastIndexOf("."));
             fileName = UUID.randomUUID().toString() + ext;
             saveFile(realpath, fileName, saveFile);
+
+            fileService.save(new File(fileName, createMd5(file).toString(), new Date()));
+
         } catch (Exception ex) {
-            System.out.println(ex);
             return "{\"error\":true}";
         }
-        try {
-            fileService.save(new File(fileName, createMd5(file).toString(), new Date()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return"{jsonrpc = \"2.0\",id = id,filePath = \"/Upload/\" + fileFullName}";}
+
+        return "{jsonrpc = \"2.0\",id = id,filePath = \"/Upload/\" + fileFullName}";
+    }
 }
 
